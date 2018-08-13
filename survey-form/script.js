489c019cf7e5ff62f157e37af6d91c4f24da2843
Mathis Boultoureau <37533818+mboultoureau@users.noreply.github.com>
2018-08-13 23:28:41 +0200

    Added checkboxes

--- survey-form/script.js
+++ survey-form/script.js
@@ -1,27 +1,32 @@
-window.onload = function() {
+window.onload = function () {
+    initFieldInputs();
+};
+
+// TEXT INPUT
+function initFieldInputs() {
     var fieldInputs = document.querySelectorAll('.field-input');
 
-    fieldInputs.forEach(fieldInput => {
+    fieldInputs.forEach(function (fieldInput) {
         var fieldClassList = fieldInput.parentElement.classList;
 
-        fieldInput.addEventListener("focus", function() {
+        fieldInput.addEventListener("focus", function () {
             fieldClassList.add("has-label");
             fieldClassList.add("is-focused");
         });
-    
-        fieldInput.addEventListener("focusout", function() {
+
+        fieldInput.addEventListener("focusout", function () {
             fieldClassList.remove("is-focused");
             if (fieldInput.value == '') {
                 fieldClassList.remove('has-label');
             }
         });
-        
-        if(fieldInput.nodeName == "TEXTAREA") {
-            fieldInput.addEventListener("keypress", function() {
-                if(fieldInput.scrollTop != 0){
+
+        if (fieldInput.nodeName == "TEXTAREA") {
+            fieldInput.addEventListener("keypress", function () {
+                if (fieldInput.scrollTop != 0) {
                     fieldInput.style.height = fieldInput.scrollHeight + "px";
                 }
             });
         }
     });
-};
+}
